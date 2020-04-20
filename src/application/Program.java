c32559fa49cd7a92e1ae17abec4d100d39ee9bb7
Bernardo Matar <bernardo.matar@gmail.com>
2020-04-20 17:20:44 -0300

    Place and Pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,11 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		ChessMatch chessMatch = new ChessMatch();
-		
-		
+
 		UI.printBoard(chessMatch.getPieces());
-		
-		
+
 	}
 
 }
